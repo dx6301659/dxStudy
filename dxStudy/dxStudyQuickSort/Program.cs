878ecdc064c885189aa55da929031272a198841a
dingxu <ding_xu@hotmail.com>
2021-03-24 17:56:08 +0800

    add quick sort method

--- dxStudy/dxStudyQuickSort/Program.cs
+++ dxStudy/dxStudyQuickSort/Program.cs
@@ -6,12 +6,61 @@ namespace dxStudyQuickSort
     {
         static void Main(string[] args)
         {
+            //int[] arrInt = { 12, 5, 2, 1, 3, 6, 9, 8, 7, 0, 11, 13 };
+            int[] arrInt = { 27, 38, 13, 49, 76, 97, 65 };
+            QuickSort(arrInt);
+
             Console.WriteLine("Hello World!");
         }
 
         static void QuickSort(int[] arrInt)
         {
+            if (arrInt == null || arrInt.Length == 0)
+                return;
 
+            QuickSort(arrInt, 0, arrInt.Length - 1);
+        }
+
+        static void QuickSort(int[] arrInt, int intLeftIndex, int intRightIndex)
+        {
+            if (intLeftIndex >= intRightIndex)
+                return;
+
+            //first round
+            int intIndex = QuickSortUnit(arrInt, intLeftIndex, intRightIndex);
+
+            //left part
+            QuickSort(arrInt, intLeftIndex, intIndex - 1);
+
+            //right part
+            QuickSort(arrInt, intIndex + 1, intRightIndex);
+        }
+
+        static int QuickSortUnit(int[] arrInt, int intLeftIndex, int intRightIndex)
+        {
+            int intTag = arrInt[intLeftIndex];
+
+            while (intLeftIndex < intRightIndex)
+            {
+                //from right to left
+                while (intLeftIndex < intRightIndex && arrInt[intRightIndex] > intTag)
+                    intRightIndex--;
+                arrInt[intLeftIndex] = arrInt[intRightIndex];
+
+                //from left to right
+                while (intLeftIndex < intRightIndex && arrInt[intLeftIndex] < intTag)
+                    intLeftIndex++;
+                arrInt[intRightIndex] = arrInt[intLeftIndex];
+            }
+            arrInt[intLeftIndex] = intTag;
+
+            foreach (int item in arrInt)
+            {
+                Console.Write($"{item}\t");
+            }
+            Console.WriteLine();
+
+            return intRightIndex;
         }
     }
 }
