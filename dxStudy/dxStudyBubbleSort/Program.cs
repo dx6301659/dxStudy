c1351eecdcf92b93a6e7e382e5dbdcaf1e098d55
dingxu <ding_xu@hotmail.com>
2021-03-31 12:47:32 +0800

    check in my code of study

--- dxStudy/dxStudyBubbleSort/Program.cs
+++ dxStudy/dxStudyBubbleSort/Program.cs
@@ -6,19 +6,20 @@ namespace dxStudyBubbleSort
     {
         static void Main(string[] args)
         {
-            int[] arrInt = { 12, 5, 2, 1, 3, 6, 9, 8, 7, 0, 11, 13 };
-            SortByBubble(arrInt);
+            //bubble sort 1
+            int[] arrInt1 = { 12, 5, 2, 1, 3, 6, 9, 8, 7, 0, 11, 13 };
+            SortByBubble1(arrInt1);
 
-            foreach (int item in arrInt)
-            {
-                Console.Write(item);
-                Console.Write("\t");
-            }
+            Console.WriteLine("**********************************************************");
 
-            Console.WriteLine("Hello World!");
+            //bubble sort 2
+            int[] arrInt2 = { 12, 5, 2, 1, 3, 6, 9, 8, 7, 0, 11, 13 };
+            SortByBubble2(arrInt2);
+
+            Console.Read();
         }
 
-        static void SortByBubble(int[] arrInt)
+        static void SortByBubble1(int[] arrInt)
         {
             if (arrInt == null || arrInt.Length == 0)
                 return;
@@ -31,6 +32,30 @@ namespace dxStudyBubbleSort
                     if (arrInt[j] > arrInt[j + 1])
                         (arrInt[j], arrInt[j + 1]) = (arrInt[j + 1], arrInt[j]);
                 }
+
+                foreach (int item in arrInt)
+                    Console.Write($"{item}\t");
+                Console.WriteLine();
+            }
+        }
+
+        static void SortByBubble2(int[] arrInt)
+        {
+            if (arrInt == null || arrInt.Length == 0)
+                return;
+
+            int intLength = arrInt.Length;
+            for (int i = 1; i < intLength; i++)
+            {
+                for (int j = 0; j < intLength - i; j++)
+                {
+                    if (arrInt[j] > arrInt[j + 1])
+                        (arrInt[j], arrInt[j + 1]) = (arrInt[j + 1], arrInt[j]);
+                }
+
+                foreach (int item in arrInt)
+                    Console.Write($"{item}\t");
+                Console.WriteLine();
             }
         }
     }
--- dxStudy/dxStudyQuickSort/Program.cs
+++ dxStudy/dxStudyQuickSort/Program.cs
@@ -6,37 +6,60 @@ namespace dxStudyQuickSort
     {
         static void Main(string[] args)
         {
-            //int[] arrInt = { 12, 5, 2, 1, 3, 6, 9, 8, 7, 0, 11, 13 };
-            int[] arrInt = { 27, 38, 13, 49, 76, 97, 65 };
-            QuickSort(arrInt);
+            //quick sort 1
+            int[] arrInt1 = { 12, 5, 2, 1, 3, 6, 9, 8, 7, 0, 11, 13 };
+            //int[] arrInt1 = { 27, 38, 13, 49, 76, 97, 65 };
+            var quickSort1 = new QuickSort1();
+            quickSort1.QuickSort(arrInt1);
 
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("*******************************************************");
+
+            //quick sort 2
+            int[] arrInt2 = { 12, 5, 2, 1, 3, 6, 9, 8, 7, 0, 11, 13 };
+            //int[] arrInt2 = { 27, 38, 13, 49, 76, 97, 65 };
+            var quickSort2 = new QuickSort2();
+            quickSort2.QuickSort(arrInt2);
+
+            Console.WriteLine("*******************************************************");
+
+            //quick sort 3
+            int[] arrInt3 = { 12, 5, 2, 1, 3, 6, 9, 8, 7, 0, 11, 13 };
+            //int[] arrInt3 = { 27, 38, 13, 49, 76, 97, 65 };
+            var quickSort3 = new QuickSort3();
+            quickSort3.QuickSort(arrInt3);
+
+            Console.WriteLine("*******************************************************");
+
+            Console.Read();
         }
+    }
 
-        static void QuickSort(int[] arrInt)
+    public class QuickSort1
+    {
+        public void QuickSort(int[] arrInt)
         {
             if (arrInt == null || arrInt.Length == 0)
                 return;
 
-            QuickSort(arrInt, 0, arrInt.Length - 1);
+            QuickSortUnit(arrInt, 0, arrInt.Length - 1);
         }
 
-        static void QuickSort(int[] arrInt, int intLeftIndex, int intRightIndex)
+        private void QuickSortUnit(int[] arrInt, int intLeftIndex, int intRightIndex)
         {
             if (intLeftIndex >= intRightIndex)
                 return;
 
             //first round
-            int intIndex = QuickSortUnit(arrInt, intLeftIndex, intRightIndex);
+            int intIndex = SortUnit(arrInt, intLeftIndex, intRightIndex);
 
             //left part
-            QuickSort(arrInt, intLeftIndex, intIndex - 1);
+            QuickSortUnit(arrInt, intLeftIndex, intIndex - 1);
 
             //right part
-            QuickSort(arrInt, intIndex + 1, intRightIndex);
+            QuickSortUnit(arrInt, intIndex + 1, intRightIndex);
         }
 
-        static int QuickSortUnit(int[] arrInt, int intLeftIndex, int intRightIndex)
+        private int SortUnit(int[] arrInt, int intLeftIndex, int intRightIndex)
         {
             int intTag = arrInt[intLeftIndex];
 
@@ -63,7 +86,7 @@ namespace dxStudyQuickSort
             return intRightIndex;
         }
 
-        static int QuickSortUnit2(int[] arrInt, int intLeftIndex, int intRightIndex)
+        private int SortUnit2(int[] arrInt, int intLeftIndex, int intRightIndex)
         {
             int intTag = arrInt[intLeftIndex];
 
@@ -79,7 +102,6 @@ namespace dxStudyQuickSort
                     intLeftIndex++;
                 (arrInt[intLeftIndex], arrInt[intRightIndex]) = (arrInt[intRightIndex], arrInt[intLeftIndex]);
             }
-            arrInt[intLeftIndex] = intTag;
 
             foreach (int item in arrInt)
             {
@@ -88,6 +110,129 @@ namespace dxStudyQuickSort
             Console.WriteLine();
 
             return intRightIndex;
+        }
+    }
+
+    public class QuickSort2
+    {
+        public void QuickSort(int[] arrInt)
+        {
+            if (arrInt == null || arrInt.Length == 0)
+                return;
+
+            QuickSortUnit(arrInt, 0, arrInt.Length - 1);
+        }
+
+        private void QuickSortUnit(int[] arrInt, int intStartIndex, int intEndIndex)
+        {
+            if (intStartIndex >= intEndIndex)
+                return;
+
+            //first round
+            int intResult = SortUnit(arrInt, intStartIndex, intEndIndex);
+
+            //left part
+            QuickSortUnit(arrInt, intStartIndex, intResult - 1);
+
+            //right part
+            QuickSortUnit(arrInt, intResult + 1, intEndIndex);
+        }
+
+        private int SortUnit(int[] arrInt, int intStartIndex, int intEndIndex)
+        {
+            int intTag = arrInt[intStartIndex];
+            while (intStartIndex < intEndIndex)
+            {
+                //from right to left
+                while (intStartIndex < intEndIndex && arrInt[intEndIndex] > intTag)
+                    intEndIndex--;
+                (arrInt[intStartIndex], arrInt[intEndIndex]) = (arrInt[intEndIndex], arrInt[intStartIndex]);
+
+                //from lef to right
+                while (intStartIndex < intEndIndex && arrInt[intStartIndex] < intTag)
+                    intStartIndex++;
+                (arrInt[intStartIndex], arrInt[intEndIndex]) = (arrInt[intEndIndex], arrInt[intStartIndex]);
+            }
+
+            foreach (int item in arrInt)
+                Console.Write($"{item}\t");
+            Console.WriteLine();
+
+            return intStartIndex;
+        }
+
+        private int SortUnit2(int[] arrInt, int intStartIndex, int intEndIndex)
+        {
+            int intTag = arrInt[intStartIndex];
+            while (intStartIndex < intEndIndex)
+            {
+                //from right to left
+                while (intStartIndex < intEndIndex && arrInt[intEndIndex] > intTag)
+                    intEndIndex--;
+                arrInt[intStartIndex] = arrInt[intEndIndex];
+
+                //from left to right
+                while (intStartIndex < intEndIndex && arrInt[intStartIndex] < intTag)
+                    intStartIndex++;
+                arrInt[intEndIndex] = arrInt[intStartIndex];
+            }
+
+            arrInt[intStartIndex] = intTag;
+
+            foreach (int item in arrInt)
+                Console.Write($"{item}\t");
+            Console.WriteLine();
+
+            return intEndIndex;
+        }
+    }
+
+    public class QuickSort3
+    {
+        public void QuickSort(int[] arrInt)
+        {
+            if (arrInt == null || arrInt.Length == 0)
+                return;
+
+            QuickSortUnit(arrInt, 0, arrInt.Length - 1);
+        }
+
+        private void QuickSortUnit(int[] arrInt, int intLeftIndex, int intRightIndex)
+        {
+            if (intLeftIndex >= intRightIndex)
+                return;
+
+            //first round
+            int intResult = SortUnit(arrInt, intLeftIndex, intRightIndex);
+
+            //left part
+            QuickSortUnit(arrInt, intLeftIndex, intResult - 1);
+
+            //right part
+            QuickSortUnit(arrInt, intResult + 1, intRightIndex);
+        }
+
+        private int SortUnit(int[] arrInt, int intLeftIndex, int intRightIndex)
+        {
+            int intTag = arrInt[intLeftIndex];
+            while (intLeftIndex < intRightIndex)
+            {
+                //from right to left
+                while (intLeftIndex < intRightIndex && arrInt[intRightIndex] > intTag)
+                    intRightIndex--;
+                (arrInt[intLeftIndex], arrInt[intRightIndex]) = (arrInt[intRightIndex], arrInt[intLeftIndex]);
+
+                //from left to right
+                while (intLeftIndex < intRightIndex && arrInt[intLeftIndex] < intTag)
+                    intLeftIndex++;
+                (arrInt[intLeftIndex], arrInt[intRightIndex]) = (arrInt[intRightIndex], arrInt[intLeftIndex]);
+            }
+
+            foreach (int item in arrInt)
+                Console.Write($"{item}\t");
+            Console.WriteLine();
+
+            return intLeftIndex;
         }
     }
 }
