3a71875e92385b7e5a5a11d7551cb1395a7e40d2
dingxu <dx6301659@163.com>
2021-07-03 13:32:05 +0800

    Check in my study code

---
+++ dxStudy/dxStudyRoute/Controllers/VisitorController.cs
@@ -0,0 +1,34 @@
+﻿using dxStudyRoute.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace dxStudyRoute.Controllers
+{
+    public class VisitorController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        //[Route("/visitor")]
+        [Route("visitor")]   
+        public ActionResult GetVisitor()
+        {
+            return Content("Ask for specific user on URL: /visitor/{visitorId}");
+        }
+
+        [HttpPost]
+        [Route("visitor/createnew")]
+        public ActionResult CreateVisitor(Visitor visitor)
+        {
+            //var visitorId = CreateVisitor(visitor);
+            return Content("Visitor creted!");
+        }
+    }
+}
---
+++ dxStudy/dxStudyRoute/Models/Visitor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace dxStudyRoute.Models
+{
+    public class Visitor
+    {
+        public string Name { get; set; }
+        public string Preference { get; set; }
+    }
+}
