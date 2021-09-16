397b88f37ae0ba14bfcae7043cf1e4d22ce958a8
丁旭 <ding_xu@hotmail.com>
2021-09-16 13:04:58 +0800

    Check in the function download files

---
+++ dxStudy/dxStudyDownloadFileByURL/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace dxStudyDownloadFileByURL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strURLFilePath = @"C:\Users\ding_\Desktop\URLPath.txt";
+            string strSavedPath = @"D:\迅雷下载\123\东北潘某\";
+
+            //read path from the file
+            using (var fs = new FileStream(strURLFilePath, FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                using (var sr = new StreamReader(fs))
+                {
+                    string str = sr.ReadLine();
+                    while (!string.IsNullOrWhiteSpace(str))
+                    {
+                        //DownloadFileAsync(str, strSavedPath);
+                        DownloadFileSync(str, strSavedPath);
+                        str = sr.ReadLine();
+                    }
+                }
+            }
+
+            Console.WriteLine("Download completed!");
+        }
+
+        static async void DownloadFileAsync(string strFileURL, string strSavePath)
+        {
+            string strExtensionName = Path.GetExtension(strFileURL);
+            string strSaveFilePath = $"{strSavePath}{Guid.NewGuid().ToString()}.{strExtensionName}";
+
+            using (var webClient = new WebClient())
+            {
+                await webClient.DownloadFileTaskAsync(strFileURL, strSaveFilePath);
+                Console.WriteLine($"Download complete :{strFileURL}");
+            }
+        }
+
+        static void DownloadFileSync(string strFileURL, string strSavePath)
+        {
+            string strExtensionName = Path.GetExtension(strFileURL);
+            string strSaveFilePath = $"{strSavePath}{Guid.NewGuid().ToString()}.{strExtensionName}";
+
+            using (var webClient = new WebClient())
+            {
+                webClient.DownloadFile(strFileURL, strSaveFilePath);
+            }
+
+            Console.WriteLine($"Download complete :{strFileURL}");
+        }
+    }
+}
