383d59e78f89d2d09f4008cf86185561ce4280cc
丁旭 <ding_xu@hotmail.com>
2022-01-01 18:35:01 +0800

    commit the code of study date format

---
+++ dxStudy/dxStudyDateFormate/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string strDateFormat1 = "yyyy-MM-dd";
+string strDateFormat2 = "YYYY-MM-dd";  // wrong format
+string strDateFormat3 = "yyyy-mm-dd";  // wrong format
+DateTime dtTest = new DateTime(2019, 12, 31);
+
+string strResult1 = dtTest.ToString(strDateFormat1);
+string strResult2 = dtTest.ToString(strDateFormat2);
+string strResult3 = dtTest.ToString(strDateFormat3);
+
+Console.WriteLine($"{strResult1}\t{strResult2}\t{strResult3}");
+
+Console.WriteLine("Hello, World!");
+
+Console.ReadKey();
