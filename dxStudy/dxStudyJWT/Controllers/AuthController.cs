cfc840e8920422d9efae412fc9fceef822994824
丁旭 <ding_xu@hotmail.com>
2023-04-24 09:04:54 +0800

    remove bin and obj

--- dxStudy/dxStudyJWT/Controllers/AuthController.cs
+++ dxStudy/dxStudyJWT/Controllers/AuthController.cs
@@ -6,38 +6,37 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 
-namespace dxStudyJWT.Controllers
+namespace dxStudyJWT.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class AuthController : ControllerBase
+    [AllowAnonymous]
+    [HttpGet]
+    public IActionResult Get(string userName, string pwd)
     {
-        [AllowAnonymous]
-        [HttpGet]
-        public IActionResult Get(string userName, string pwd)
+        if (string.IsNullOrWhiteSpace(userName) || string.IsNullOrWhiteSpace(pwd))
+            return BadRequest(new { message = "username or password is incorrect" });
+
+        var claims = new[]
         {
-            if (string.IsNullOrWhiteSpace(userName) || string.IsNullOrWhiteSpace(pwd))
-                return BadRequest(new { message = "username or password is incorrect" });
+            new Claim(JwtRegisteredClaimNames.Nbf, $"{new DateTimeOffset(DateTime.Now).ToUnixTimeSeconds()}"),
+            new Claim(JwtRegisteredClaimNames.Exp, $"{new DateTimeOffset(DateTime.Now.AddMinutes(30)).ToUnixTimeSeconds()}"),
+            // new Claim(ClaimTypes.Name, userName), //添加这一行则会把user name放入生成的token中的payload
+            new Claim(ClaimTypes.NameIdentifier, userName)
+        };
 
-            var claims = new[]
-            {
-                new Claim(JwtRegisteredClaimNames.Nbf, $"{new DateTimeOffset(DateTime.Now).ToUnixTimeSeconds()}"),
-                new Claim(JwtRegisteredClaimNames.Exp, $"{new DateTimeOffset(DateTime.Now.AddMinutes(30)).ToUnixTimeSeconds()}"),
-                // new Claim(ClaimTypes.Name, userName), //添加这一行则会把user name放入生成的token中的payload
-                new Claim(ClaimTypes.NameIdentifier, userName)
-            };
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Cont.SecurityKey));
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+        var token = new JwtSecurityToken(
+            issuer: Cont.Domain,
+            audience: Cont.Domain,
+            claims: claims,
+            expires: DateTime.Now.AddMinutes(30),
+            signingCredentials: creds
+            );
 
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Cont.SecurityKey));
-            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
-            var token = new JwtSecurityToken(
-                issuer: Cont.Domain,
-                audience: Cont.Domain,
-                claims: claims,
-                expires: DateTime.Now.AddMinutes(30),
-                signingCredentials: creds
-                );
-
-            return Ok(new { token = new JwtSecurityTokenHandler().WriteToken(token) });
-        }
+        return Ok(new { token = new JwtSecurityTokenHandler().WriteToken(token) });
     }
 }
--- dxStudy/dxStudyJWT/Controllers/ValuesController.cs
+++ dxStudy/dxStudyJWT/Controllers/ValuesController.cs
@@ -3,27 +3,26 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
 
-namespace dxStudyJWT.Controllers
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ValuesController : ControllerBase
-    {
-        [HttpGet]
-        [Route("value1")]
-        public ActionResult<IEnumerable<string>> Get()
-        {
-            return new string[] { "value1", "value1" };
-        }
+namespace dxStudyJWT.Controllers;
 
-        [HttpGet]
-        [Route("value2")]
-        [Authorize]
-        public ActionResult<IEnumerable<string>> Get2()
-        {
-            var auth = HttpContext.AuthenticateAsync();
-            var userName = auth.Result.Principal?.Claims.First(t => t.Type.Equals(ClaimTypes.NameIdentifier))?.Value;
-            return new string[] { "value1", "value2", $"userName: {userName}" };
-        }
+[Route("api/[controller]")]
+[ApiController]
+public class ValuesController : ControllerBase
+{
+    [HttpGet]
+    [Route("value1")]
+    public ActionResult<IEnumerable<string>> Get()
+    {
+        return new string[] { "value1", "value1" };
+    }
+
+    [HttpGet]
+    [Route("value2")]
+    [Authorize]
+    public ActionResult<IEnumerable<string>> Get2()
+    {
+        var auth = HttpContext.AuthenticateAsync();
+        var userName = auth.Result.Principal?.Claims.First(t => t.Type.Equals(ClaimTypes.NameIdentifier))?.Value;
+        return new string[] { "value1", "value2", $"userName: {userName}" };
     }
 }
